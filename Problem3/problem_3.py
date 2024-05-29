3d362f66ecd5f628a0d1baa6fb73a4563216ab40
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-05-29 12:16:29 +0100

    problem 1

---
+++ Problem3/problem_3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Problem3: a script that take a list and multiply it's values by 10
+"""
+
+ur_list = [1, 2, 3, 4, 5]
+r = list()
+print("The result: ")
+for elem in ur_list:
+    print(f"{elem*10}")
+
