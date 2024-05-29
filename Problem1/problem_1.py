cc5f33fdc41e67c12b6c3e86835216b65a62c1b9
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2024-05-29 11:54:03 +0100

    problem 1

---
+++ Problem1/problem_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+The problem 1 say to store tow variables and make a simple output
+"""
+
+fav_num = 22
+fav_str = "Morocco"
+print(f"My favorite number is {fav_num} and my favorite string is {fav_str}")
